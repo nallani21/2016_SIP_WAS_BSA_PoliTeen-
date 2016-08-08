5d09c29dbae16ac00e5191d6c91a9ec8d4b367cb
nallani21 <nallani21@gmail.com>
2016-08-08 15:28:26 -0400

    all nidhis stuff

--- javaPT.js
+++ javaPT.js
@@ -1,25 +1,16 @@
-function initMap() {
-        var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {
-            center: {lat: 41, lng: -105},
-            zoom: 4,
-            mapTypeId: 'hybrid'
-        });
-          
-       var marker = new google.maps.Marker({
-          map: map,
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          position: {lat: 39.154207, lng: -77.290642}
-        });
-        marker.addListener('click', toggleBounce);
+$(document).ready(function(){
+  $('#map').usmap({
 
+    'mouseoverState':{
+      'HI': function(event,data) {
+        // body...
       }
-
-      function toggleBounce() {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
+    },
+    'click' : function(event, data){
+      $('#alert')
+        .text('Click '+ data.name+'')
+        .stop()
+        .css('backgroundColor', '#ffffff')
       }
+    });
+  });
